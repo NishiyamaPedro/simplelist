debcf598afaa55b5898c539f2e45ffbb2b4fe5bc
Pedro Nishiyama <nishiyama.pedro@gmail.com>
2020-09-12 04:12:59 -0300

    novo layout e melhorias

--- app/src/main/java/com/nishiyama/lista/ListaAdapter.kt
+++ app/src/main/java/com/nishiyama/lista/ListaAdapter.kt
@@ -1,36 +1,39 @@
 package com.nishiyama.lista
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
-import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.NumberPicker
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.collections.ArrayList
 
 class ListaAdapter(val context : Context, val ListaList: ArrayList<ListaController>) : RecyclerView.Adapter<ListaAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListaAdapter.ViewHolder {
-        val v = LayoutInflater.from(parent?.context).inflate(R.layout.list_row, parent, false)
-        return ViewHolder(v);
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.list_row, parent, false)
+        return ViewHolder(v)
     }
 
+    @SuppressLint("DefaultLocale")
     override fun onBindViewHolder(holder: ListaAdapter.ViewHolder, position: Int) {
-        holder?.chkb?.text = ListaList[position].name.capitalize()
-        holder?.chkb?.isChecked = ListaList[position].chkd
-        holder?.quantidade?.text = ListaList[position].quantidade.toString()
-        holder?.vtot?.text = String.format("%.2f", (ListaList[position].valor * ListaList[position].quantidade))
-        holder?.valo?.text = String.format("%.2f", ListaList[position].valor)
-        holder?.lItem?.setOnLongClickListener(View.OnLongClickListener { view ->
-            val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+        holder.chkb.text = ListaList[position].name.capitalize()
+        holder.chkb.isChecked = ListaList[position].chkd
+        holder.quantidade.text = ListaList[position].quantidade.toString()
+        holder.vtot.text = String.format("%.2f", (ListaList[position].valor * ListaList[position].quantidade))
+        holder.valo.text = String.format("%.2f", ListaList[position].valor)
+        holder.lItem.setOnLongClickListener {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(context, R.style.MyDialogTheme)
             builder.setTitle("Deseja deletar o item?")
 
-            builder.setPositiveButton("Sim",
-                DialogInterface.OnClickListener { dialog, which ->
+            builder.setPositiveButton("Sim"
+            ){ _, _ ->
                     if(ListaList[position].chkd) {
                         listen.value = -(ListaList[position].valor * ListaList[position].quantidade)
                     }
@@ -38,61 +41,62 @@ class ListaAdapter(val context : Context, val ListaList: ArrayList<ListaControll
                     ListaList.removeAt(position)
                     notifyItemRemoved(position)
                     notifyItemRangeChanged(position, ListaList.size)
-                })
-            builder.setNegativeButton("Não",
-                DialogInterface.OnClickListener { dialog, which ->
+                }
+            builder.setNegativeButton("Não"
+            ) { dialog, _ ->
                     dialog.cancel()
-                })
+                }
             builder.show()
 
             true
-        })
+        }
 
-        holder?.chkb?.setOnClickListener(View.OnClickListener { view ->
+        holder.chkb.setOnClickListener { view ->
             if(view is CheckBox){
                 if(view.isChecked){
-                    val builder: AlertDialog.Builder = AlertDialog.Builder(context)
-                    builder.setTitle("Insira o preço")
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(context, R.style.MyDialogTheme)
+                    builder.setTitle("Preço do(a) ${ListaList[position].name.capitalize()}")
 
                     val customLayout: View = LayoutInflater.from(context).inflate(R.layout.dpreco, null)
                     val npick: NumberPicker = customLayout.findViewById(R.id.numberpick1)
                     val input: EditText = customLayout.findViewById(R.id.editText)
                     input.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
-                    npick.setMaxValue(99);
-                    npick.setMinValue(1);
+                    npick.maxValue = 99
+                    npick.minValue = 1
                     builder.setView(customLayout)
 
 
-                    builder.setOnCancelListener(DialogInterface.OnCancelListener {
+
+                    builder.setOnCancelListener {
                         view.isChecked = false
                         ListaList[position].chkd = false
-                    })
-                    builder.setPositiveButton("Inserir",
-                        DialogInterface.OnClickListener { dialog, which ->
-                            if(input.text.toString() != "") {
-                                ListaList[position].quantidade = npick.value
-                                ListaList[position].valor = input.text.toString().toDouble()
-                            } else {
-                                ListaList[position].valor = 0.00
-                                ListaList[position].quantidade = 0
-                                dialog.cancel()
-                            }
-                            ListaList[position].chkd = true
-                            listen.value = (ListaList[position].valor * ListaList[position].quantidade)
-                            notifyItemRangeChanged(0, ListaList.size);
-
-                        })
-                    builder.setNeutralButton("Usar anterior",
-                        DialogInterface.OnClickListener { dialog, which ->
-                            ListaList[position].chkd = true
-                            listen.value = (ListaList[position].valor * ListaList[position].quantidade)
-                            notifyItemRangeChanged(0, ListaList.size);
-
-                        })
-                    builder.setNegativeButton("Cancelar",
-                        DialogInterface.OnClickListener { dialog, which ->
+                    }
+                    builder.setPositiveButton("Inserir"
+                    ) { dialog, _ ->
+                        if(input.text.toString() != "") {
+                            ListaList[position].quantidade = npick.value
+                            ListaList[position].valor = input.text.toString().toDouble()
+                        } else {
+                            ListaList[position].valor = 0.00
+                            ListaList[position].quantidade = 0
                             dialog.cancel()
-                        })
+                        }
+                        ListaList[position].chkd = true
+                        listen.value = (ListaList[position].valor * ListaList[position].quantidade)
+                        notifyItemRangeChanged(0, ListaList.size)
+
+                    }
+                    builder.setNeutralButton("Usar anterior"
+                    ) { _, _ ->
+                        ListaList[position].chkd = true
+                        listen.value = (ListaList[position].valor * ListaList[position].quantidade)
+                        notifyItemRangeChanged(0, ListaList.size)
+
+                    }
+                    builder.setNegativeButton("Cancelar"
+                    ) { dialog, _ ->
+                        dialog.cancel()
+                    }
                     builder.show()
                 } else {
                     if(!view.isChecked) {
@@ -101,13 +105,13 @@ class ListaAdapter(val context : Context, val ListaList: ArrayList<ListaControll
                     }
                 }
             }
-        })
+        }
     }
 
-    public fun saveList() {
+    fun saveList() {
         val dataProccessor = DataProcess(context)
         dataProccessor.clean()
-        for((j, i) in ListaList.withIndex()){
+        for((j, _) in ListaList.withIndex()){
             dataProccessor.setStr("nome${j}", ListaList[j].name )
             dataProccessor.setStr("valor${j}", ListaList[j].valor.toString() )
             dataProccessor.setBool("ch${j}", ListaList[j].chkd)
@@ -115,14 +119,14 @@ class ListaAdapter(val context : Context, val ListaList: ArrayList<ListaControll
         }
     }
 
-    public fun loadList() {
+    fun loadList() {
         val dataProccessor = DataProcess(context)
         for(j in 0 until dataProccessor.getSize()){
             ListaList.add(ListaController(dataProccessor.getStr("nome${j}")!!,dataProccessor.getStr("valor${j}")!!.toDouble(),dataProccessor.getBool("ch${j}"),dataProccessor.getInt("qtd${j}")))
         }
     }
-    public fun loadPreco() {
-        for((j, i) in ListaList.withIndex())
+    fun loadPreco() {
+        for((j, _) in ListaList.withIndex())
             if (ListaList[j].chkd)
                 listen.value = (ListaList[j].valor * ListaList[j].quantidade)
     }
@@ -132,10 +136,10 @@ class ListaAdapter(val context : Context, val ListaList: ArrayList<ListaControll
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val chkb = itemView.findViewById<CheckBox>(R.id.checkBox)
-        val valo = itemView.findViewById<TextView>(R.id.textView2)
-        val lItem = itemView.findViewById<ConstraintLayout>(R.id.Litem)
-        val quantidade = itemView.findViewById<TextView>(R.id.textView8)
-        val vtot = itemView.findViewById<TextView>(R.id.textView4)
+        val chkb: CheckBox = itemView.findViewById(R.id.checkBox)
+        val valo: TextView = itemView.findViewById(R.id.textView2)
+        val lItem: ConstraintLayout = itemView.findViewById(R.id.Litem)
+        val quantidade: TextView = itemView.findViewById(R.id.textView8)
+        val vtot: TextView = itemView.findViewById(R.id.textView4)
     }
 }
--- app/src/main/java/com/nishiyama/lista/MainActivity.kt
+++ app/src/main/java/com/nishiyama/lista/MainActivity.kt
@@ -1,98 +1,229 @@
 package com.nishiyama.lista
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
-import android.view.*
+import android.view.Gravity
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.input.*
 
 
-val listen : MutableLiveData<Double> =  MutableLiveData<Double>()
+val listen : MutableLiveData<Double> =  MutableLiveData()
 
 class MainActivity : AppCompatActivity() {
-    var recyclerView: RecyclerView? = null
     var lista = ArrayList<ListaController>()
-    var adapter = ListaAdapter(this, lista)
-    var total: Double = 0.00
+    private var adapter = ListaAdapter(this, lista)
+    private var total: Double = 0.00
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.salvar -> {
-                adapter.saveList()
-                ShowTost(this,"Lista salva!")
-                true
-            }
-            R.id.carregar -> {
-                lista.clear()
-                adapter.loadList()
-                total = 0.0
-                adapter.loadPreco()
-                adapter.notifyItemRangeChanged(0, lista.size);
-                ShowTost(this,"Lista carregada!")
-                true
-            }
-            R.id.limpar -> {
-                lista.clear()
-                valTotal.text = "0,00"
-                adapter.notifyDataSetChanged()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
+    @RequiresApi(Build.VERSION_CODES.M)
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         val rvRecyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         rvRecyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         rvRecyclerView.adapter = adapter
 
-        button6.setOnClickListener {
+
+        fab.setOnClickListener {
+            //addLista()
+            fab.visibility = View.INVISIBLE
+            coordinatorLayout.visibility = View.INVISIBLE
+            input.visibility = View.VISIBLE
+            inputEdit.requestFocus()
+
+            val constraintLayout: ConstraintLayout = findViewById(R.id.mainlayout)
+            val constraintSet = ConstraintSet()
+            constraintSet.clone(constraintLayout)
+            constraintSet.connect(
+                R.id.recyclerView,
+                ConstraintSet.BOTTOM,
+                R.id.input,
+                ConstraintSet.TOP,
+                0
+            )
+            constraintSet.applyTo(constraintLayout)
+
+            val imm =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+        }
+
+        btnAdd.setOnClickListener {
             addLista()
         }
-        edit1.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+
+        btnSair.setOnClickListener {
+            fab.visibility = View.VISIBLE
+            coordinatorLayout.visibility = View.VISIBLE
+            input.visibility = View.INVISIBLE
+
+            val constraintLayout: ConstraintLayout = findViewById(R.id.mainlayout)
+            val constraintSet = ConstraintSet()
+            constraintSet.clone(constraintLayout)
+            constraintSet.connect(
+                R.id.recyclerView,
+                ConstraintSet.BOTTOM,
+                R.id.coordinatorLayout,
+                ConstraintSet.TOP,
+                0
+            )
+            constraintSet.applyTo(constraintLayout)
+
+            val imm = getSystemService(
+                Context.INPUT_METHOD_SERVICE
+            ) as InputMethodManager
+            imm.hideSoftInputFromWindow(inputEdit.windowToken, 0)
+        }
+
+        inputEdit.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
                 addLista()
+
                 return@OnKeyListener true
             }
             false
         })
 
         listen.observe(this, Observer {
-            total += listen.value!!.toDouble()
-            if (total < 0) {
-                total = 0.0
+            if (lista.isNotEmpty()) {
+                total += listen.value!!.toDouble()
+                if (total < 0) {
+                    total = 0.0
+                }
+                valTotal2.text = String.format("%.2f", total)
             }
-            valTotal.text = String.format("%.2f", total)
+        })
+
+        bottomAppBar.setOnMenuItemClickListener { item ->
+            if (item != null) when (item.itemId) {
+                R.id.salvar -> {
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(this, R.style.MyDialogTheme)
+                    builder.setTitle("Deseja salvar?")
+
+                    builder.setPositiveButton("Sim"
+                    ){ _, _ ->
+                        if (lista.isNotEmpty()) {
+                            adapter.saveList()
+                            showToast(this, "Lista salva!")
+                        }   else {
+                            showToast(this, "Lista em branco! Não foi possível salvar.")
+                        }
+                    }
+                    builder.setNegativeButton("Não"
+                    ) { dialog, _ ->
+                        dialog.cancel()
+                    }
+                    builder.show()
+
+                }
+                R.id.carregar -> {
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(this, R.style.MyDialogTheme)
+                    builder.setTitle("Deseja carregar?")
+
+                    builder.setPositiveButton("Sim"
+                    ){ _, _ ->
+                        val dataProcess = DataProcess(this)
+                        if (dataProcess.getSize() != 0) {
+                            lista.clear()
+                            adapter.loadList()
+                            total = 0.0
+                            valTotal2.text = "0,00"
+                            adapter.loadPreco()
+                            adapter.notifyItemRangeChanged(0, lista.size)
+                            textView11.visibility = View.INVISIBLE
+                            imageView.visibility = View.INVISIBLE
+                            showToast(this, "Lista carregada!")
+                        } else {
+                            showToast(this, "Não há lista salva.")
+                        }
+                    }
+                    builder.setNegativeButton("Não"
+                    ) { dialog, _ ->
+                        dialog.cancel()
+                    }
+                    builder.show()
+
+                }
+                R.id.limpar -> {
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(this, R.style.MyDialogTheme)
+                    builder.setTitle("Limpar lista atual?")
+
+                    builder.setPositiveButton("Sim"
+                    ){ _, _ ->
+                        lista.clear()
+                        total = 0.0
+                        valTotal2.text = "0,00"
+                        adapter.notifyDataSetChanged()
+                        textView11.visibility = View.VISIBLE
+                        imageView.visibility = View.VISIBLE
+                    }
+                    builder.setNegativeButton("Não"
+                    ) { dialog, _ ->
+                        dialog.cancel()
+                    }
+                    builder.show()
 
 
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+            true
+        }
+        this?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (input.visibility == View.VISIBLE){
+                    fab.visibility = View.VISIBLE
+                    coordinatorLayout.visibility = View.VISIBLE
+                    input.visibility = View.INVISIBLE
+
+                    val constraintLayout: ConstraintLayout = findViewById(R.id.mainlayout)
+                    val constraintSet = ConstraintSet()
+                    constraintSet.clone(constraintLayout)
+                    constraintSet.connect(
+                        R.id.recyclerView,
+                        ConstraintSet.BOTTOM,
+                        R.id.coordinatorLayout,
+                        ConstraintSet.TOP,
+                        0
+                    )
+                    constraintSet.applyTo(constraintLayout)
+                }
+            }
         })
     }
 
-    fun ShowTost(context: Context?, ToastMessage: String?) {
+    private fun showToast(context: Context?, ToastMessage: String?) {
         val toast = Toast.makeText(context, ToastMessage, Toast.LENGTH_SHORT)
         toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0)
         toast.show()
     }
-    fun addLista() {
-        if (edit1.text.isNotBlank()) {
-            lista.add(ListaController(edit1.text.toString(), 0.00,false,0))
+    private fun addLista() {
+        if (inputEdit.text.isNotBlank()) {
+            lista.add(ListaController(inputEdit.text.toString(), 0.00,false,0))
             adapter.notifyItemInserted(lista.size - 1)
-            edit1.setText("")
+            inputEdit.setText("")
         }
+        textView11.visibility = View.INVISIBLE
+        imageView.visibility = View.INVISIBLE
     }
 }
 
